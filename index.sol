6827eb6aa369dc955a4fee3c3ff9bf4e2b2eeb9c
Brandon Arias Cjuno <brandon12arias@gmail.com>
2022-02-05 12:06:02 -0500

    Estructura y funcion de creacion de zombies basica

--- index.sol
+++ index.sol
@@ -1,6 +1,14 @@
 //SPDX-License-Ientifier: MIT
-pragma solidiy ^0.8.0
+pragma solidity ^0.8.3;
 
-contract init{
+contract ZombieFactory{
+    struct Zombie{
+        string name;
+        uint dna;
+    }
 
+    Zombie[] Cuartel;
+    function CrearZombie(string calldata _name, uint _dna) public {
+        Cuartel.push(Zombie("prueva", 1));
+    }
 }
