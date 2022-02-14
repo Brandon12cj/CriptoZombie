3354f49aff4b635d5e08a5bd6c0094ac52032232
Brandon Arias Cjuno <brandon12arias@gmail.com>
2022-02-14 09:30:55 -0500

    logre hacer funcionar el codigo

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,14 +1,26 @@
 //SPDX-License-Ientifier: MIT
-pragma solidity ^0.8.3;
+pragma solidity ^0.4.19;
 
 contract ZombieFactory{
+    uint digitNumber = 16;
+    uint dnaModulus = 10 ** digitNumber;
+
     struct Zombie{
         string name;
         uint dna;
     }
 
-    Zombie[] Cuartel;
-    function CrearZombie(string calldata _name, uint _dna) public {
-        Cuartel.push(Zombie("prueva", 1));
+    Zombie[] public Cuartel;
+    function CrearZombie(string _name, uint _dna) private {
+        Cuartel.push(Zombie(_name, _dna));
+    }
+    function genRandom(string name) private returns(uint){
+        uint lol = uint(keccak256(name));
+        uint dna = lol % dnaModulus;
+        return dna;
+    }
+    function RandomZombie(string _nombre) public {
+        uint dnaRandom = genRandom(_nombre);
+        CrearZombie(_nombre, dnaRandom);
     }
 }
