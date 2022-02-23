b07f861bf1337e95c4cfaf6af7c2acff9de814e9
Brandon Arias Cjuno <brandon12arias@gmail.com>
2022-02-23 10:44:23 -0500

    obtimice la funcion feedandmultiply

--- FeedAndMultipli.sol
+++ FeedAndMultipli.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.4.19;
 import "./zombiefactory.sol";
 
 contract FeedAndMultipli is ZombieFactory {
-    function alimentarse(uint _id, uint _dna) public view returns(uint) {
+    function alimentarse(uint _id, uint _dna) public {
     uint dnaNewZb = (zombies[_id].dna + _dna) / 2;
-    return dnaNewZb;
+    _createZombie("NoName", dnaNewZb);
     } 
 }
